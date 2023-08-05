4ec03720f7b237c9c79794011a74f4393c019898
Wator Vapor <watorvapor@gmail.com>
2023-08-05 15:45:22 +0900

    emqx cluster

--- emqx-cluster/create.selfcert.sh
+++ emqx-cluster/create.selfcert.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 mkdir -p ./self-certs/
-openssl genrsa -des3 -out ./self-certs/rootCA.key 2048
+cd ./self-certs/
 pwd
-openssl req -x509 -new -nodes -key ./self-certs/rootCA.key -sha256 -days 3650 -out ./self-certs/rootCA.crt
-pwd
-openssl genrsa -out ./self-certs/server.key 2048
-pwd
-openssl req -new -key ./self-certs/server.key -out ./self-certs/server.csr
-pwd
-openssl x509 -req -in ./self-certs/server.csr -CA ./self-certs/rootCA.crt -CAkey ./self-certs/rootCA.key -CAcreateserial -out ./self-certs/server.crt -days 365
+openssl genrsa -des3 -out ./rootCA.key 2048
+openssl req -x509 -new -nodes -key ./rootCA.key -sha256 -days 3650 -out ./rootCA.crt
+openssl genrsa -out ./server.key 2048
+openssl req -new -key ./server.key -out ./server.csr
+openssl x509 -req -in ./server.csr -CA ./rootCA.crt -CAkey ./rootCA.key -CAcreateserial -out ./server.crt -days 365
+cd -
 pwd

340988df50fffe516e9100c9257c456ccaa539b2
Wator Vapor <watovapor@gmail.com>
2023-11-11 15:53:27 +0000

    addrefsdafss weak verify

---
+++ otmc-portal/index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const MqttJWTDidTeam = require('./mqtt/did_team.js');
+const secretKeyPath = './.store//secretKey/auth.json';
+const strConstMqttJwtPath = './.store/mqtt/jwt_cached.json';
+
+let secretKey = null;
+try {
+  const secretText = fs.readFileSync(secretKeyPath);
+  secretKey = JSON.parse(secretText);
+} catch ( err ) {
+  console.log('::::err=<',err,'>');  
+}
+//console.log('::::secretKey=<',secretKey,'>');
+
+let topDidDoc = null;
+let didManifest = null;
+try {
+  const strConstTopDidDocPath = `./.store/didteam/${secretKey.idOfKey}/topDocument.json`;
+  const topDidDocText = fs.readFileSync(strConstTopDidDocPath);
+  topDidDoc = JSON.parse(topDidDocText);
+  const strConstManifestPath = `./.store/didteam/${secretKey.idOfKey}/manifest.json`;
+  const manifestText = fs.readFileSync(strConstManifestPath);
+  didManifest = JSON.parse(manifestText);
+
+} catch ( err ) {
+  console.log('::::err=<',err,'>');
+}
+//console.log('::::topDidDoc=<',topDidDoc,'>');
+
+
+
+let jwtCached = null;
+try {
+  fs.mkdirSync('./.store/mqtt/', { recursive: true });
+  const jwtText = fs.readFileSync(strConstMqttJwtPath);
+  jwtCached = JSON.parse(jwtText);
+} catch ( err ) {
+  console.log('::::err=<',err,'>');    
+}
+//console.log('::::jwtCached=<',jwtCached,'>');
+
+const team = new MqttJWTDidTeam(jwtCached,topDidDoc,didManifest,secretKey,(jwtRcv) => {
+  onRecvedJwtReply(jwtRcv);
+});
+
+const onRecvedJwtReply = (jwtRcv) => {
+  fs.writeFileSync(strConstMqttJwtPath, JSON.stringify(jwtRcv,undefined,2));  
+}

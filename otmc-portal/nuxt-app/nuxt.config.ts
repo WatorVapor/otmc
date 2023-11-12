90cc4663db31614db6a4b59c03791e1939ac799c
Wator Vapor <watovapor@gmail.com>
2023-11-12 14:36:03 +0000

    use ejs

--- otmc-portal/nuxt-app/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  server: {
-    socket: '/dev/shm/otmc.portal.nuxt.socket'
-  }
-})
-
-// npm run dev -–unix-socket=/dev/shm/otmc.portal.nuxt.socket

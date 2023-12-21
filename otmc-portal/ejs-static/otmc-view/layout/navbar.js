17b7a203f1d6d6d94d4042e69f697d89eceb4578
Wator Vapor <watovapor@gmail.com>
2023-12-21 14:18:33 +0000

    otmc ejs

--- otmc-portal/ejs-static/otmc-view/layout/navbar.js
+++ otmc-portal/ejs-static/otmc-view/layout/navbar.js
@@ -1,35 +1,42 @@
+import * as Vue from 'vue';
 const NAVBAR = {
-  trace:false,
+  trace:true,
   debug:true,
 }
 window.addEventListener('DOMContentLoaded', async (evt) => {
   createTopNavBar_();
 });
+const navbarOption = {
+  data() {
+    return {
+      accout: {
+        name:'',
+      }
+    };
+  },
+  methods: {
+    onClickChangeLang(lang) {
+      if(NAVBAR.trace) {
+        console.log('w-navbar::onClickChangeLang::lang=<',lang,'>');
+      }    
+      localStorage.setItem(constKeyLanguangeCode,lang);
+      location.reload(true);
+    },
+  }  
+}
 
 const createTopNavBar_ = async ()=> {
-  const { default:createVueApp } = await import(`${constAppPrefix}/assets/component/navbar.js`);
-  if(NAVBAR.trace) {
-    console.log('w-navbar::createTopNavBar_::createVueApp=<',createVueApp,'>');
-  }
-  const app = await createVueApp(gNavbarMethods);
+  const app = Vue.createApp(navbarOption);
   const vm = app.mount('#vue-navbar-top');  
   if(NAVBAR.trace) {
     console.log('w-navbar::createTopNavBar_::vm=<',vm,'>');
   }
+  /*
   window.vueVm = window.vueVm || {};
   window.vueVm.navbar = vm;
 
   const evt = document.createEvent('Event');
   evt.initEvent('TopMenuBarLoaded', true, true);
   document.dispatchEvent(evt);
+  */
 }
-
-const gNavbarMethods = {
-  onClickChangeLang(lang) {
-    if(NAVBAR.trace) {
-      console.log('w-navbar::onClickChangeLang::lang=<',lang,'>');
-    }    
-    localStorage.setItem(constKeyLanguangeCode,lang);
-    location.reload(true);
-  },
-};

dace957332e658ca536b8d7b93c919f9981ebd94
Wator Vapor <watovapor@gmail.com>
2024-02-11 23:28:12 +0000

    ejs-static

--- otmc-portal/ejs-element-plus-static/otmc-html/layout/navbar.js
+++ otmc-portal/ejs-element-plus-static/otmc-html/layout/navbar.js
@@ -1,19 +1,59 @@
 import * as Vue from 'vue';
+import * as ElementPlus from 'elementPlus';
+import * as ElementPlusLocaleZhCn from 'elementPlusZh';
+//import { default  } from 'elementPlusZh';
+
+
 const NAVBAR = {
   trace:true,
   debug:true,
 }
+const activeIndex = Vue.ref('1')
+const handleSelect = (key, keyPath) => {
+  if(NAVBAR.trace) {
+    console.log('w-navbar::handleSelect::key=<',key,'>');
+    console.log('w-navbar::handleSelect::keyPath=<',keyPath,'>');
+  }
+}
+
+if(NAVBAR.trace) {
+  console.log('w-navbar::::Vue=<',Vue,'>');
+  console.log('w-navbar::::activeIndex=<',activeIndex,'>');
+  console.log('w-navbar::::ElementPlus=<',ElementPlus,'>');
+  console.log('w-navbar::::ElementPlusLocaleZhCn=<',ElementPlusLocaleZhCn,'>');
+}
+
 window.addEventListener('DOMContentLoaded', async (evt) => {
   createTopNavBar_();
 });
+
+const epOption = {
+  locale: ElementPlusLocaleZhCn.default
+};
+
+if(NAVBAR.trace) {
+  console.log('w-navbar::::epOption=<',epOption,'>');
+}
+
 const appOption = {
   data() {
     return {
       app:{
         
       },
+      accout: {
+        name:'',
+      },
     };
   },
+  methods: {
+    handleSelect(lang) {
+      if(NAVBAR.trace) {
+        console.log('w-navbar::handleSelect::lang=<',lang,'>');
+      }    
+      const activeIndex = Vue.ref('1');
+    },
+  }
 }
 
 const langOption = {
@@ -45,11 +85,12 @@ const teamOption = {
 
 const createTopNavBar_ = async ()=> {
   const app = Vue.createApp(appOption);
-  const vmApp = app.mount('#vue-ui-navbar-top-app');  
+  app.use(ElementPlus,epOption);
+  const vmApp = app.mount('#vue-ui-navbar');  
   if(NAVBAR.trace) {
     console.log('w-navbar::createTopNavBar_::vmApp=<',vmApp,'>');
   }
-
+/*
   const lang = Vue.createApp(langOption);
   const vmLang = lang.mount('#vue-ui-navbar-top-lang');  
   if(NAVBAR.trace) {
@@ -61,7 +102,7 @@ const createTopNavBar_ = async ()=> {
   if(NAVBAR.trace) {
     console.log('w-navbar::createTopNavBar_::vmTeam=<',vmTeam,'>');
   }
-
+*/
   
   /*
   window.vueVm = window.vueVm || {};

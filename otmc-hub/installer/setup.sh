0524ad9390a0891b79b034b4e34c501342ae41ac
Wator Vapor <watorvapor@gmail.com>
2024-03-05 16:02:24 +0900

    setup

--- otmc-hub/installer/setup.sh
+++ otmc-hub/installer/setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+TTY_CONF=/etc/systemd/system/getty@tty59.service.d/override.conf
 sudo apt-get update && sudo apt-get install -y docker.io docker-compose
 sudo usermod -aG docker $USER
+sudo touch ${TTY_CONF}
+sudo echo "" >${TTY_CONF}
+sudo echo "[Service]" >>${TTY_CONF}
+sudo echo "ExecStart=" >>${TTY_CONF}
+sudo echo "ExecStart=-/sbin/agetty -a ${USER} --noclear %I $TERM" >>${TTY_CONF}

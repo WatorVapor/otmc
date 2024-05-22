bb237cded55659a3a546cd9cd2ee051bd2a8c73e
wator vapor <watorvapor@gmail.com>
2024-05-22 22:21:44 +0000

    join as auth

--- otmc-hub/gateway-node/bin/join.auth.sh
+++ otmc-hub/gateway-node/bin/join.auth.sh
@@ -2,4 +2,4 @@
 source ../docker.env.sh
 echo "DOCKER_NODE:=${DOCKER_NODE}"
 ${DOCKER_NODE} node \
-  ../cli/join.team.mjs --address 'did:otmc:otmsnaftnd45lzlcdrsqpr73zzst3okf'
+  ../cli/join.auth.mjs --address 'did:otmc:otmsnaftnd45lzlcdrsqpr73zzst3okf'

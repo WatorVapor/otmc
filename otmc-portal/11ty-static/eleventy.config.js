a1b3576bbc07628e804186e9305bdae3f9e02c58
wator vapor <watorvapor@gmail.com>
2024-08-30 12:59:06 +0000

    global vars

--- otmc-portal/11ty-static/eleventy.config.js
+++ otmc-portal/11ty-static/eleventy.config.js
@@ -2,9 +2,10 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.setEjsOptions({
 		delimiter: '%',
 	});
-  eleventyConfig.addWatchTarget('templete_views/*');
+  eleventyConfig.addWatchTarget('templete_views');
   eleventyConfig.setWatchThrottleWaitTime(100); // in milliseconds
   eleventyConfig.addPassthroughCopy('templete_views/**/*.js');
+  eleventyConfig.addGlobalData('prefix', '');
   const config = {
     dir: {
       input: 'templete_views',

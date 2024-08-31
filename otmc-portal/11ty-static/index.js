e5c8f25755c4d98c4b9c410d7f2eb8b3475dcfdb
wator vapor <watorvapor@gmail.com>
2024-08-31 06:55:30 +0000

    ssg on watch

---
+++ otmc-portal/11ty-static/index.js
@@ -0,0 +1,36 @@
+LOG = {
+  trace:false
+};
+const chokidar = require('chokidar');
+const execSync = require('child_process').execSync;
+// One-liner for current directory
+const inputWatcher = chokidar.watch('./templete_views',{
+  persistent: true,
+  usePolling: true,
+  interval: 1000,
+});
+inputWatcher.on('all', (event, path) => {
+  onInputChanged(event, path);
+});
+
+const gTimeoutIDs = [];
+const onInputChanged = (event, path) => {
+  //console.log('onInputChanged::event:=<',event ,'>');
+  //console.log('onInputChanged::path:=<',path ,'>');
+  for(const tId of gTimeoutIDs) {
+    clearTimeout(tId);
+  }
+  if(gTimeoutIDs.length > 0) {
+    gTimeoutIDs.splice(0,gTimeoutIDs.length);
+  }
+  const timeId = setTimeout(()=>{
+    execChangedEvt(path);
+  },1000);
+  gTimeoutIDs.push(timeId);
+}
+
+const execChangedEvt = (path) => {
+  console.log('execChangedEvt::path:=<',path ,'>');
+  const result = execSync(`npm run build`);
+  console.log('execChangedEvt::result:=<',result.toString('utf-8') ,'>');
+}

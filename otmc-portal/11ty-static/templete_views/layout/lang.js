7388d3b0efaa877d4d7d6116e5faa268affd0f32
Wator Vapor <watovapor@gmail.com>
2024-08-31 07:15:59 +0000

    http proxy

---
+++ otmc-portal/11ty-static/templete_views/layout/lang.js
@@ -0,0 +1,67 @@
+import * as Vue from 'vue';
+document.addEventListener('TopMenuBarLoaded', (evt) => {
+  createMultiLanguage_();
+});
+const multiLang = {
+  trace:false,
+  debug:true,
+};
+
+const appLangList = [];
+const createMultiLanguage_ = async () => {
+  let lang = localStorage.getItem(constKeyLanguangeCode);
+  if(!lang) {
+    lang = 'cn';
+  }
+  if(multiLang.trace) {
+    console.log('::location=<',location,'>');
+    console.log('::lang=<',lang,'>');
+  }
+  const langURL = `${location.pathname}lang_${lang}.js`;
+  if(multiLang.trace) {
+    console.log('::langURL=<',langURL,'>');
+  }
+  const langPromise = import(langURL);
+  const langModule = await langPromise;
+  if(multiLang.trace) {
+    console.log('::langModule.data=<',langModule.data,'>');
+  }
+  const langCommonURL = `${constAppPrefix}/layout/lang_${lang}.js`;
+  if(multiLang.trace) {
+    console.log('::langCommonURL=<',langCommonURL,'>');
+  }
+  const langCommonPromise = import(langCommonURL);
+  const langCommonModule = await langCommonPromise;
+  if(multiLang.trace) {
+    console.log('::langCommonModule.data=<',langCommonModule.data,'>');
+  }
+  const allData = {...langModule.data, ...langCommonModule.data}
+  if(multiLang.trace) {
+    console.log('::allData=<',allData,'>');
+  }
+  const langElem = document.querySelectorAll('.vue-lang');
+  if(multiLang.trace) {
+    console.log('createMultiLanguage_::langElem=<',langElem,'>');
+  }
+  langElem.forEach((el, i) => {
+    if(multiLang.trace) {
+      console.log('createMultiLanguage_::el=<',el,'>');
+      console.log('createMultiLanguage_::allData=<',allData,'>');
+    }
+    const app = Vue.createApp({
+      data() {
+        return allData;
+      },
+      delimiters:['{%', '%}']
+    });
+    const vm = app.mount(el);
+    appLangList.push(vm);
+  });  
+}
+
+window.updateMultiLanguage = () => {
+  if(multiLang.trace) {
+    console.log('updateMultiLanguage::=<','','>');
+  }
+  createMultiLanguage_();
+}

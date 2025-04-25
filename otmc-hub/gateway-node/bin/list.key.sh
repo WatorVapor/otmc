bf4d362db8935a508921ef05984c4fd69fda4e8a
wator vapor <watorvapor@gmail.com>
2025-04-25 14:05:07 +0000

    run in nodejs

---
+++ otmc-hub/gateway-node/bin/list.key.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_WHERE=$(cd "$(dirname "$0")" && pwd)
+echo "SCRIPT_WHERE: $SCRIPT_WHERE"
+cd $SCRIPT_WHERE
+source ../docker.env.sh
+echo "DOCKER_NODE:=${DOCKER_NODE}"
+${DOCKER_NODE} node \
+  ./cli/cli.mjs --subcommand ${0} \
+  --address $1 \
+  || true
+cd -

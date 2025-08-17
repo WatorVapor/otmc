a792895c171e4c7c7ce7d62bf110e100d5daafa7
wator vapor <watorvapor@gmail.com>
2025-08-17 14:11:53 +0000

    rtk

--- otmc-hub/rtk-gnss-py/main.py
+++
@@ -1,8 +0,0 @@
-from serial import Serial
-from pyrtcm import RTCMReader
-with Serial('/dev/ttyUSB0', 115200, timeout=3) as stream:
-  while True :
-    rtr = RTCMReader(stream)
-    raw_data, parsed_data = rtr.read()
-    if parsed_data is not None:
-      print('::parsed_data=<',parsed_data,'>')
--- otmc-hub/rtk-gnss-py/run.x.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source ./docker.env.sh
-echo "DOCKER_PYTHON:=${DOCKER_PYTHON}"
-${DOCKER_PYTHON} /bin/bash
